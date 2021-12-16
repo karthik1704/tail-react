c0b1ebb4fe23ee8b97048f26a0002182e32b125c
Karthik A <karthikthee7@gmail.com>
2021-12-16 20:11:38 +0530

    Add Navbar to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <Navbar/>
       <h1 className=' font-poppins text-3xl font-bold text-purple-500'> Hello world!!!</h1>
     </div>
   )
